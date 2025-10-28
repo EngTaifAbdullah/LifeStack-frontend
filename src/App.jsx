02f46d4cbf2289011508b5242729b9067fc1b233
taif <eng.taifabdullah@gmail.com>
2025-10-29 00:10:08 +0300

    Edit ROUTERS for certificate page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar/NavBar";
-import CertificatesList from "./components/Certificate/CertificateForm";
-import CertificateForm from "./components/Certificate/CertificatesList";
+import CertificateForm from "./components/Certificate/CertificateForm";
+import CertificateList from "./components/Certificate/CertificateList";
+import CertificateView from "./components/Certificate/CertificateView";
 
-import './App.css'
+// import './App.css'
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<CertificatesList />} />
-        <Route path="/add" element={<CertificateForm />} />
-        <Route path="/edit/:id" element={<CertificateForm />} />
+        <Route path="/" element={<CertificateList />} />
+        <Route path="/certificate" element={<CertificateList />} />
+        <Route path="/certificate/new" element={<CertificateForm />} />
+        <Route path="/certificate/:certId/edit" element={<CertificateForm />} />
+        <Route path="/certificate/:certId" element={<CertificateView />} />
       </Routes>
     </Router>
   );
--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,10 @@
-// inested by writing evry time
+// inested by writing API every time
+
 
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "http://127.0.0.1:8000/api", 
 });
 
 export default api;
