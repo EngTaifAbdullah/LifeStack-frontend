f1cc0fc5507cc376bcd6dff07edd42f5bcdfe2d4
taif <eng.taifabdullah@gmail.com>
2025-10-28 19:06:04 +0300

    Update APP.JSX and CREATE API folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,22 @@
-import React from 'react'
-import axios from 'axios'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar/NavBar";
+import CertificatesList from "./components/Certificate/CertificateForm";
+import CertificateForm from "./components/Certificate/CertificatesList";
 
-import NavBar from './components/NavBar/NavBar'
-import'.App.css'
-
-
-// first components
-import CertificateIndex from './components/Certificate/CertificateIndex'
-import CertificateDetail from './components/Certificate/CertificateDetail'
-import CertificateForm from './components/Certificate/CertificateForm'
+import './App.css'
 
 function App() {
-
-  async function makeRequest() {
-    
-    try {
-      const response = await axios.get('http://127.0.0.1:8000/api/certificates/')
-      console.log(response.data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  makeRequest()
-
   return (
     <Router>
-      <NavBar />
+      <Navbar />
       <Routes>
-        <Route path="/certificates" element={<CertificateIndex />} />
-        <Route path="/certificates/new" element={<CertificateForm />} />
-        <Route path="/certificates/:cert_id" element={<CertificateDetail />} />
-        <Route path="/certificates/:cert_id/edit" element={<CertificateForm />} />
+        <Route path="/" element={<CertificatesList />} />
+        <Route path="/add" element={<CertificateForm />} />
+        <Route path="/edit/:id" element={<CertificateForm />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
