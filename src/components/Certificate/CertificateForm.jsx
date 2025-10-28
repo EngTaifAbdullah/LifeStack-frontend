499227352fef10b6b117e1699bb6e90e78407c7d
taif <eng.taifabdullah@gmail.com>
2025-10-29 00:36:39 +0300

    Creating theFull CRUD for the Certificate Form AND add some actions such as handelchange and handelsubmit

---
+++ src/components/Certificate/CertificateForm.jsx
@@ -0,0 +1,183 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import api from "../../api/api";
+import "../../App.css";
+
+// _________________________________________________________________________________________________________________________________
+
+function CertificateForm() {
+
+  const navigate = useNavigate();
+  const { certId } = useParams();
+
+  const [formData, setFormData] = useState({
+
+    title: "",
+    organization: "",
+    date_obtained: "",
+    file: null,
+
+  });
+
+  const [fileName, setFileName] = useState("");
+  const [existingFileUrl, setExistingFileUrl] = useState("");       // To save the linke for previos file
+
+  useEffect(() => {
+    if (certId) {
+      getCertificate();
+    }
+  }, [certId]);
+
+// _________________________________________________________________________________________________________________________________
+
+  const getCertificate = async () => {
+
+
+    try {
+      const response = await api.get(`/certificate/${certId}/`);
+      setFormData({
+
+        title: response.data.title,
+        organization: response.data.organization,
+        date_obtained: response.data.date_obtained,
+        file: null, 
+      });
+
+      setFileName(response.data.file ? response.data.file.split("/").pop() : "");
+      setExistingFileUrl(response.data.file || ""); 
+      
+    } catch (error) {
+      console.error("Error fetching certificate:", error);
+    }
+  };
+
+  // _________________________________________________________________________________________________________________________________
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+
+    
+    if (name === "file") {
+      setFormData({ ...formData, file: files[0] });
+      setFileName(files[0]?.name || "");
+
+
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+
+  // _________________________________________________________________________________________________________
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const data = new FormData();
+    data.append("title", formData.title);
+    data.append("organization", formData.organization);
+    data.append("date_obtained", formData.date_obtained);
+    data.append("user", 1); // to pass the user id
+
+    // if the user want to update the privios file do it ! if not..dont do anything
+
+    if (formData.file) {
+      data.append("file", formData.file);
+    }
+
+    try {
+
+      if (certId) {
+      
+        await api.patch(`/certificate/${certId}/`, data, {     //  edite certificate
+          headers: { "Content-Type": "multipart/form-data" },
+        });
+        alert("Certificate has Edit Successfully");  //message
+      } 
+      
+      else {
+
+        await api.post("/certificate/", data, {      // to add new certificate
+          headers: { "Content-Type": "multipart/form-data" },
+        });
+        alert("Certificate has Added Successfully");
+      }
+
+
+      navigate("/certificate");
+    } catch (error) {
+      console.error("Error saving certificate:", error.response?.data || error);
+      alert("Oops! something wrong");
+    }
+  };
+
+  // _________________________________________________________________________________________________________
+  
+  return (
+    <div className="main-content">
+      <div className="form-container">
+        <h1>{certId ? "Update Certificate" : "Add New Certificate "}</h1>
+
+
+
+        <form onSubmit={handleSubmit}>
+          <label>Certificate Title</label>
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+          />
+
+
+
+          <label>Organization</label>
+          <input
+            type="text"
+            name="organization"
+            value={formData.organization}
+            onChange={handleChange}
+            required
+          />
+
+
+
+          <label>Date Obtained</label>
+          <input
+            type="date"
+            name="date_obtained"
+            value={formData.date_obtained}
+            onChange={handleChange}
+            required
+          />
+
+
+
+          <label>Upload File</label>
+          <input type="file" name="file" onChange={handleChange} />
+          
+          {/* To viwe privios file you updated*/}
+          
+          {fileName && !formData.file && (
+            <p>
+               Current File :{" "}
+              <a href={existingFileUrl} target="_blank" rel="noopener noreferrer">
+                {fileName}
+              </a>
+            </p>
+          )}
+
+
+
+     
+          {formData.file && <p>📁 New File: {formData.file.name}</p>}
+
+          <button type="submit">{certId ? "Edit" : "Add"}</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default CertificateForm;
