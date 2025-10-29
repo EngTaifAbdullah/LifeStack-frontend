271a65dc090b54dac84022bd341cc8550f0aafe0
taif <eng.taifabdullah@gmail.com>
2025-10-29 16:57:50 +0300

    Create Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar/NavBar";
+
+import Home from "./components/Home/HomePage";
+
 import CertificateForm from "./components/Certificate/CertificateForm";
 import CertificateList from "./components/Certificate/CertificateList";
 import CertificateView from "./components/Certificate/CertificateView";
@@ -9,13 +12,12 @@ import CertificateView from "./components/Certificate/CertificateView";
 import './App.css'
 
 
-
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<CertificateList />} />
+        <Route path="/" element={<Home />} />
         <Route path="/certificate" element={<CertificateList />} />
         <Route path="/certificate/new" element={<CertificateForm />} />
         <Route path="/certificate/:certId/edit" element={<CertificateForm />} />
---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// _________________________________________________________________________________________________________
+
+function Home() {
+  return (
+
+    <div className="home-container">
+
+      <div className="welcome-card">
+        <h1> Welcome to LifeStack Websit 🍓📑 </h1>
+        <p>
+          tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
+        </p>
+      </div>
+{/* ____________________________________________________ */}
+
+      <div className="home-info">
+        <h2>🚀 Quick Overview</h2>
+
+        <ul>
+          <li>TAIF ABDULLAH</li>
+          <li>TAIF ABDULLAH TAIF ABDULLAH</li>
+          <li>TAIF ABDULLAH TAIF ABDULLAH TAIF ABDULLAH </li>
+        </ul>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;
